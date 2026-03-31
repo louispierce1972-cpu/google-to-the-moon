c45e330e20b38edc865db709d0ddff1bdcd69f9a
Dima <dima@gmail.com>
2026-03-31 20:40:28 +0700

    update

--- card-tracker/proxy.js
+++ card-tracker/proxy.js
@@ -27,7 +27,9 @@ const server = http.createServer((req, res) => {
             path: viperPath,
             method: req.method,
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Accept': 'application/json, text/plain, */*'
             },
             timeout: 300000
         };
